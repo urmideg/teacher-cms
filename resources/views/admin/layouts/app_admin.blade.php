b9fd1caf5fbaa15bbfd3358f8c21f9df1d5507a0
Ivan Leonov <leonov.k13@yandex.ru>
2018-02-18 12:03:29 +0300

    Admin: article index page

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -47,7 +47,7 @@
 
                           <ul class="dropdown-menu" role="menu">
                             <li><a href="{{route('admin.category.index')}}">Категории</a></li>
-                            <li><a href="#">Материалы</a></li>
+                            <li><a href="{{route('admin.article.index')}}">Записи блога</a></li>
                           </ul>
                         <li>
                     </ul>
