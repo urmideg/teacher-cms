f54793cbaf5201a19bedb3334ad017bce3418de9
Ivan Leonov <leonov.k13@yandex.ru>
2018-02-19 21:32:45 +0300

    Admin: article destroy

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -103,6 +103,8 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        $article->categories()->detach();
+        $article->delete();
+        return redirect()->route('admin.article.index');
     }
 }
