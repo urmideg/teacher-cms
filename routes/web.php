c9acdb53a30e5ce13858c4abb3e448fb46e4f0b7
Ivan Leonov <leonov.k13@yandex.ru>
2018-02-20 15:56:57 +0300

    Created menu for blog

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['aut
 });
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.home');
 });
 
 Auth::routes();
