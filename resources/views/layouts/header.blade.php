1a839236c6d0d89b0395b05158d6bc1acf114f9f
Leonov Ivan <leonov.k13@yandex.ru>
2018-03-16 15:47:28 +0300

    Enable/disable user registration in config

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,7 +27,9 @@
                 <!-- Authentication Links -->
                 @guest
                     <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    @if (config('auth.registration'))
+                      <li><a href="{{ route('register') }}">Register</a></li>
+                    @endif
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,26 @@ Route::get('/', function () {
     return view('blog.home');
 });
 
-Auth::routes();
+// Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+if (config('auth.registration')) {
+    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    $this->post('register', 'Auth\RegisterController@register');
+} else {
+    Route::match(['get','post'], 'register', function () {
+        return redirect()->route('login');
+    })->name('register');
+}
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
